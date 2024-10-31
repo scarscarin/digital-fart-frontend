714097252a455e46a2e9befbef556a14f6b0313d
scarscarin <scarscarin@gmail.com>
2024-10-31 18:56:51 +0100

    leo is an idiot

--- archive.js
+++ archive.js
@@ -46,6 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 playAudio(index);
             });
 
+            rectangle.addEventListener('touchstart', () => {
+                playAudio(index);
+            });
+
             // Update progress overlay during playback
             audio.addEventListener('timeupdate', () => {
                 const progress = (audio.currentTime / audio.duration) * 100;
